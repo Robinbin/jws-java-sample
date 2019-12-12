4667188f454f5eac7713b667bf11e53638eabab1
Jack Zhu <hansenzhulei@gmail.com>
2019-12-12 21:54:45 +0800

    added jenkins

--- jdk/src/main/java/com/jws/samples/leetcode/algorithm/Solution.java
+++ jdk/src/main/java/com/jws/samples/leetcode/algorithm/Solution.java
@@ -17,4 +17,19 @@ public class Solution {
 
         return result;
     }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null;
+
+        return result;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
 }
